098145445342d8d13093d899326ad6082d951405
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2022-11-15 09:13:25 +0100

    * first import * add a script for making the students version (without solution blocks)

---
+++ scripts/makeStudentsVersion.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os, sys
+import subprocess
+import glob
+
+nInputParams = len(sys.argv)
+input_file_name = ""
+if nInputParams<2:
+    print("No input file proivided! Usage:")
+    print("makeStudentsVersion.py notebook.ipynb")
+    exit(1)
+else:
+    input_file_name = sys.argv[1]
+    
+    
+    
+
+print("Modyfying file:","blue", input_file_name)
+exit(0)
+output_file = open("tmp.ipynb", "w")
+result = subprocess.run(["awk", " /#BEGIN_SOLUTION/{p=1}/#END_SOLUTION/{p=0;print \"    \\\"...\\\\n\\\", \";next}!p", input_file_name],
+                        text=True, stdout=output_file)
+
+subprocess.run(["mv","tmp.ipynb",input_file_name])
+    
+print("Done.")
+
